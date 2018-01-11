63802714385cc755b7bba38971c67ea656cb20c4
Unknown <m.bury@stud.elka.pw.edu.pl>
2018-01-11 21:40:50 +0100

    działa ładowanie obrazku

--- example/faceRecog/faceDataRead01.m
+++ example/faceRecog/faceDataRead01.m
@@ -1,11 +1,15 @@
 mltRoot = 'C:/Users/Misiek/Desktop/eiasr/example/faceRecog';
-imageDir=[mltRoot, '/dataSet/att_faces'];
-faceData=recursiveFileList(imageDir, 'pgm');
+imageDir=[mltRoot, '/dataSet'];
+faceData=recursiveFileList(imageDir, 'jpg');
 fprintf('Reading %d face images from %s...', length(faceData), imageDir);
 tic
 for i=1:length(faceData)
 % 	fprintf('%d/%d: file=%s\n', i, length(faceData), faceData(i).path);
-	faceData(i).image=imread(faceData(i).path);
+	
+    X = imread(faceData(i).path);
+    
+    imwrite(X,'test.pgm');
+    faceData(i).image=X;
 end
 fprintf(' ===> %.2f sec\n', toc);
 fprintf('Saving faceData.mat...\n');
--- example/faceRecog/faceDisplay01.m
+++ example/faceRecog/faceDisplay01.m
@@ -3,5 +3,7 @@ subplot(1,2,1);
 imagesc(faceData(1).image); colormap(gray)
 axis image;
 subplot(1,2,2);
-surf(double(faceData(1).image)); colormap(gray)
+temp = double(faceData(1).image);
+surf(temp); 
+colormap(gray);
 axis image; shading interp; view(140, 80);
